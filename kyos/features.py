89b3dedf302bd737a0473e2e56117bd6a91d9b79
Nathan Xue <nathan.xue@fda.hhs.gov>
2018-12-12 14:28:51 -0500

    Fixed issue with reading insertions

--- kyos/features.py
+++ kyos/features.py
@@ -71,10 +71,10 @@ def read_summary_file(summary_file):
         for line in f:
             z = line.split()
             key = z[0] + " " + z[1]
-            truth_var = z[3].upper()
+            truth_var = z[3]
             if truth_var.endswith("_insertion"): #Strip off reference from insertions
                 truth_var = truth_var[1:]
-            truth[key] = [z[2].upper(), z[3]]
+            truth[key] = [z[2].upper(), truth_var]
 
     return truth
 
